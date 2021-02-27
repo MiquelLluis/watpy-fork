e938aa91081ae67caeaea47a9763c4397158c7d0
Matteo Breschi <matteo.breschi@ligo.org>
2021-02-27 09:01:52 +0100

    remove declare_namespace

--- watpy/base/__init__.py
+++ watpy/base/__init__.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python
-from __future__ import absolute_import, unicode_literals
-__import__("pkg_resources").declare_namespace(__name__)
 
 from . import wave, utils, units, phys_utils, gw_utils, num_utils
--- watpy/db_utilities/__init__.py
+++ watpy/db_utilities/__init__.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python
-from __future__ import absolute_import, unicode_literals
-__import__("pkg_resources").declare_namespace(__name__)
 
 from . import dbutils, h5_utils, index, metadata_utils, simlist_utils, viz_utils
